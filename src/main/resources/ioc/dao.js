b9d6a6627b58881618f54f651d442faffd60689a
maodj <maodj@tico.cn>
2019-07-12 22:40:01 +0800

    数据库变更

--- src/main/resources/ioc/dao.js
+++ src/main/resources/ioc/dao.js
@@ -6,11 +6,11 @@ var ioc = {
             depose : 'close'
         },
         fields : {
-            url : "jdbc:mysql://127.0.0.1:3306/zhihu_test",
+            url : "jdbc:mysql://172.16.16.13:3306/zhihu_test",
             username : "root",
                 password : "123456",
             testWhileIdle : true, // 非常重要,预防mysql的8小时timeout问题
-            //validationQuery : "select 1" , // Oracle的话需要改成 select 1 from dual
+            validationQuery : "select 1" , // Oracle的话需要改成 select 1 from dual
             maxActive : 100
         }
     },
